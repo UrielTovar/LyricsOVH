475fbecd435ebe300666445703276767856bc6bd
Uriel Tovar <ult_18@me.com>
2020-08-11 15:06:27 -0500

    Project structure and dependencies

---
+++ app/src/main/java/com/tovar/lyricsovh/presentation/search_song_list/SearchSongActivity.kt
@@ -0,0 +1,12 @@
+package com.tovar.lyricsovh.presentation.search_song_list
+
+import android.os.Bundle
+import com.tovar.lyricsovh.R
+import com.tovar.lyricsovh.presentation.core.BaseActivity
+
+class SearchSongActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_song)
+    }
+}
---
+++ app/src/main/java/com/tovar/lyricsovh/presentation/song_lyric/SongLyricActivity.kt
@@ -0,0 +1,12 @@
+package com.tovar.lyricsovh.presentation.song_lyric
+
+import android.os.Bundle
+import com.tovar.lyricsovh.R
+import com.tovar.lyricsovh.presentation.core.BaseActivity
+
+class SongLyricActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_lyric)
+    }
+}
